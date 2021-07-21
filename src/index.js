47b026ac74aa4b9747cc740dd2c186f671be4433
Ilya <ilyapavlushchik221@gmail.com>
2021-07-21 19:30:24 +0300

    Сделал калькулятор
    -При смене месяцев проиходит смена цены
    -Так же она происходит при смене Клубныая
    -При вводе промокода цена уменьщается на 30%

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@
 import chooseClub from './modules/chooseClub';
 import togglePopUp from './modules/togglePopUp';
 import sendForm from './modules/sendForm';
+import calc from './modules/calc';
 
 chooseClub();
 
 togglePopUp();
 
 sendForm();
+
+calc();
---
+++ src/modules/calc.js
@@ -0,0 +1,58 @@
+const calc = () => {
+  const mozaikaPrices = {
+    1: '1999',
+    6: '9900',
+    9: '13990',
+    12: '19900',
+  };
+
+  const schelkovoPrices = {
+    1: '2999',
+    6: '14990',
+    9: '21990',
+    12: '24990',
+  };
+
+  const priceValues = document.querySelectorAll('input[name="card-type"]'),
+    clubs = document.querySelectorAll('[data-club]'),
+    form = document.getElementById('card_order'),
+    priceMessage = document.getElementById('price-total'),
+    promo = document.getElementById('promo');
+
+  const showPrice = (obj, month, discount) => {
+    Object.keys(obj).map((price) => {
+      if (month === price)
+        priceMessage.textContent = Math.floor(+obj[price] * discount);
+    });
+  };
+
+  const checkClub = (month, discount = 1) => {
+    clubs.forEach((club, i) => {
+      if (club.checked) {
+        i === 0 && showPrice(mozaikaPrices, month, discount);
+        i === 1 && showPrice(schelkovoPrices, month, discount);
+      }
+    });
+  };
+
+  const checkPriceValues = (discount) => {
+    priceValues.forEach((item) => {
+      item.checked && checkClub(item.value, discount);
+    });
+  };
+
+  form.addEventListener('input', (e) => {
+    const target = e.target;
+    if (target.matches('[data-price]')) {
+      checkClub(target.value);
+    }
+    if (target.matches('[data-club]')) {
+      checkPriceValues();
+    }
+    if (promo.value === 'ТЕЛО2019') {
+      checkPriceValues(0.7);
+    }
+  });
+};
+
+export default calc;
